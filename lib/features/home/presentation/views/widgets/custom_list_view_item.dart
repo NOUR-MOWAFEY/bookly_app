ebed9e4cfbaf30e20d83339e569e01bc755d9b72
Nour Mowafey <nourmowafey82@gmail.com>
2026-01-24 22:32:08 +0200

    add futured list view to home view

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,23 +1,20 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FuturedListViewItem extends StatelessWidget {
+  const FuturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(AssetsData.testImage),
-              fit: BoxFit.fill,
-            ),
-            borderRadius: BorderRadius.circular(16),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AssetsData.testImage),
+            fit: BoxFit.fill,
           ),
+          borderRadius: BorderRadius.circular(16),
         ),
       ),
     );
---
+++ lib/features/home/presentation/views/widgets/futured_books_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FuturedBooksListView extends StatelessWidget {
+  const FuturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: FuturedListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
