0f10c369f24d21aed6d9587d439efbcd708efa00
Nour Mowafey <nourmowafey82@gmail.com>
2026-01-25 23:06:59 +0200

    create similar books list view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.
 import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,32 +13,55 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          const SizedBox(height: 28),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.18),
-            child: const CustomBookItem(),
-          ),
-          const SizedBox(height: 38),
-          Text('The Jungle Book', style: Styles.textStyle30),
-          const SizedBox(height: 6),
-          Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18.copyWith(
-              color: Color(0xFFB7B6BC),
-              fontStyle: FontStyle.italic,
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: CustomBookDetailsAppBar(),
+        ),
+        const SizedBox(height: 20),
+
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.18 + 30),
+          child: const CustomBookItem(),
+        ),
+        const SizedBox(height: 38),
+
+        Text('The Jungle Book', style: Styles.textStyle30),
+        const SizedBox(height: 4),
+
+        Text(
+          'Rudyard Kipling',
+          style: Styles.textStyle18.copyWith(color: Color(0xFFB7B6BC)),
+        ),
+        const SizedBox(height: 14),
+
+        const BookRating(),
+        const SizedBox(height: 39),
+
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: BooksAction(),
+        ),
+        const SizedBox(height: 50),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
             ),
           ),
-          const SizedBox(height: 12),
-          const BookRating(),
-          const SizedBox(height: 39),
-          const BooksAction(),
-        ],
-      ),
+        ),
+        const SizedBox(height: 18),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: const SimilarBooksListView(),
+        ),
+      ],
     );
   }
 }
