f7cfadd6726ecc4e9fdd768aa372f81900290ec0
Nour Mowafey <nourmowafey82@gmail.com>
2026-01-25 23:16:22 +0200

    use custom scroll view in book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -13,53 +13,62 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
 
-    return Column(
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: CustomBookDetailsAppBar(),
-        ),
-        const SizedBox(height: 20),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: CustomBookDetailsAppBar(),
+              ),
+              const SizedBox(height: 20),
 
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * 0.18 + 30),
-          child: const CustomBookItem(),
-        ),
-        const SizedBox(height: 38),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.18 + 30),
+                child: const CustomBookItem(),
+              ),
+              const SizedBox(height: 38),
 
-        Text('The Jungle Book', style: Styles.textStyle30),
-        const SizedBox(height: 4),
+              Text('The Jungle Book', style: Styles.textStyle30),
+              const SizedBox(height: 4),
 
-        Text(
-          'Rudyard Kipling',
-          style: Styles.textStyle18.copyWith(color: Color(0xFFB7B6BC)),
-        ),
-        const SizedBox(height: 14),
+              Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(color: Color(0xFFB7B6BC)),
+              ),
+              const SizedBox(height: 14),
 
-        const BookRating(),
-        const SizedBox(height: 39),
+              const BookRating(),
+              const SizedBox(height: 39),
 
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: BooksAction(),
-        ),
-        const SizedBox(height: 50),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: BooksAction(),
+              ),
+              Expanded(child: const SizedBox(height: 50)),
 
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle16.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 18),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: const SimilarBooksListView(),
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
-        ),
-        const SizedBox(height: 18),
-
-        Padding(
-          padding: const EdgeInsets.only(left: 30),
-          child: const SimilarBooksListView(),
         ),
       ],
     );
