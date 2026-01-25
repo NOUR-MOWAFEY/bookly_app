01855a9da2443620136fb639b2d64086079da026
Nour Mowafey <nourmowafey82@gmail.com>
2026-01-25 21:22:30 +0200

    add title and subtitle and book rating to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -8,16 +10,29 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookDetailsAppBar(),
-          SizedBox(height: 28),
+          const CustomBookDetailsAppBar(),
+          const SizedBox(height: 28),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * 0.18),
-            child: CustomBookItem(),
+            child: const CustomBookItem(),
           ),
+          const SizedBox(height: 38),
+          Text('The Jungle Book', style: Styles.textStyle30),
+          const SizedBox(height: 6),
+          Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              color: Color(0xFFB7B6BC),
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          const SizedBox(height: 12),
+          const BookRating(),
         ],
       ),
     );
